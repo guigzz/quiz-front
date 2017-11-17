7ecbcd36884dd6e6dad0d6f337167a70f7ea4257
Guillaume <guillaume.sallaber@gmail.com>
2017-11-17 09:21:09 +0100

    fetch all quiz data into QuizContent component

--- src/components/QuizContent/QuizContent.js
+++ src/components/QuizContent/QuizContent.js
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import './QuizContent.css';
 
 class QuizContent extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      id: null,
+      title: null,
+      questions: []
+    }
+  }
+
+  componentDidMount() {
+    const quizId = this.props.match.params.id;
+    console.log("will fetch quiz " + quizId);
+    
+    fetch('http://localhost/?data=quiz&id=' + quizId)
+    .then( response => response.json())
+    .then( ({id, title, questions}) => {
+      return this.setState({ // populate the state will render the component
+        id: id, 
+        title: title,
+        questions: questions
+      });
+    });
+  }
+
   render() {
     return (
       <p className="home-content">
-        This is the content of quiz {this.props.match.params.id}.
+        This is the content of quiz {this.state.id} : {this.state.title}.
+        <br />
+        <ul>
+          {this.state.questions.map((question) => {
+          return (
+            <li>{question.number}. {question.text}
+              <ul>
+                {question.choices.map((choice) => {
+                  return (
+                    <li>{choice.id} - {choice.text}</li>
+                  )
+                })}
+              </ul>
+            </li>
+          )})}
+          </ul>
       </p>
     );
   }
