0290421ea2c9645b4318a55459b6004418217245
Guillaume <guillaume.sallaber@gmail.com>
2017-11-21 19:28:09 +0100

    oops, remove unreachable code

--- src/components/ResultItem/ResultItem.js
+++ src/components/ResultItem/ResultItem.js
@@ -37,23 +37,6 @@ class ResultItem extends Component {
         
       </div>
     );
-
-
-
-    return (
-      <div className={`result-item ${resultClass}`}>
-        <div className="result-question">Question {this.props.questionNumber}. {this.props.questionText}</div>
-        <div className="result-user-answer">Your answer: <strong>{this.props.userAnswerNumber.toUpperCase()}. {this.props.userAnswerText}</strong></div>
-        <div className="result-verdict">
-          {
-            goodAnswer ? 
-            <p><strong>Well done!</strong></p>
-            : 
-            <p>Oh no... the answer was: <strong>{this.props.goodAnswerNumber.toUpperCase()}. {this.props.goodAnswerText}</strong></p>
-          }
-        </div>
-      </div>
-    );
   }
 }
 
