7d4de0a801bbbf01cd2a207c2a3ca389935ff54d
Guillaume <guillaume.sallaber@gmail.com>
2017-11-22 14:17:29 +0100

    make the app title clickable, as the home button

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './AppHeader.css';
 
 class AppHeader extends Component {
   render() {
     return (
       <header className="app-header">
-        <h1 className="app-header-title">The Awesome Quiz</h1>
+        <Link to="/"><h1 className="app-header-title">The Awesome Quiz</h1></Link>
       </header>
     );
   }
